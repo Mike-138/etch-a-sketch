086e8592b163b7e9a7cf7a227b7da748a0f951f7
Michael <mikemcd.bbs@gmail.com>
2023-09-11 16:37:14 -0400

    Add input field

--- scripts.js
+++ scripts.js
@@ -2,7 +2,6 @@ const sketchContainer = document.querySelector(".sketch-container");
 
 for (let i = 0; i < 256; i++) {
     let unitSquare = document.createElement("div");
-    unitSquare.style.cssText = "width: 25px; aspect-ratio: 1; border: 1px solid black; background: grey; box-sizing: border-box;";
+    unitSquare.classList.add("unit-square");
     sketchContainer.appendChild(unitSquare);
-    console.log("LOOP");
 }

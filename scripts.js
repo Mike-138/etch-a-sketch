275ca47667c4b6ed3ad2c2ee0dd31f3c1bd798a9
Michael <mikemcd.bbs@gmail.com>
2023-09-12 00:23:53 -0400

    Implement dynamic input for grid size
    Range is from 1 - 64 unit squares

--- scripts.js
+++ scripts.js
@@ -1,13 +1,21 @@
+function generateEtch(grid) {
+    const unitWidth = String(400 / grid);
+    console.log(unitWidth);
+    for (let i = 0; i < grid ** 2; i++) {
+        let unitSquare = document.createElement("div");
+        unitSquare.classList.add("unit-square");
+        unitSquare.style.width = unitWidth + "px";
+        sketchContainer.appendChild(unitSquare);
+        console.log(unitSquare.style)
+    }
+}
+
 const sketchContainer = document.querySelector(".sketch-container");
 const gridSizeInput = document.querySelector("input");
 
 gridSizeInput.addEventListener("input", () => {
+    sketchContainer.innerHTML = "";
     let gridSize = gridSizeInput.valueAsNumber;
     console.log(gridSize);
+    generateEtch(gridSize);
 })
-
-for (let i = 0; i < 256; i++) {
-    let unitSquare = document.createElement("div");
-    unitSquare.classList.add("unit-square");
-    sketchContainer.appendChild(unitSquare);
-}

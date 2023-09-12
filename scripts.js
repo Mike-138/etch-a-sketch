51ed75de15f986d7b33083a4e6561b243adfade2
Michael <mikemcd.bbs@gmail.com>
2023-09-11 20:53:01 -0400

    Add javascript to retrieve input value
    Need to handle case where there is no input (NaN)

--- scripts.js
+++ scripts.js
@@ -1,4 +1,10 @@
 const sketchContainer = document.querySelector(".sketch-container");
+const gridSizeInput = document.querySelector("input");
+
+gridSizeInput.addEventListener("input", () => {
+    let gridSize = gridSizeInput.valueAsNumber;
+    console.log(gridSize);
+})
 
 for (let i = 0; i < 256; i++) {
     let unitSquare = document.createElement("div");
